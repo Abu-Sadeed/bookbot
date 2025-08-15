9b910d4353777ca057668058d942df0bdd1b1d83
Sadeed <abusadeed@iut-dhaka.edu>
2025-08-15 19:39:29 +0600

    dynamic book place holder

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 
-from stats import get_word_count
+from stats import get_word_count, get_frequencies, get_frequency_list
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_location = sys.argv[1]
+
 
 def get_book_text():
-    with open("books/frankenstein.txt", "r") as f:
+    with open(book_location, "r") as f:
         return f.read()
+    
+def sort_frequencies(frequencies):
+    return frequencies["count"]
 
 words = get_book_text()
-print(f"{get_word_count(words)} words found in the document")
+
+word_count = get_word_count(words)
+frequencies = get_frequencies(words)
+frequencies_list = get_frequency_list(words)
+frequencies_list.sort( key=sort_frequencies, reverse=True)
+
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {book_location}")
+
+print("----------- Word Count ----------")
+print(f"Found {word_count} total words")
+print("--------- Character Count -------")
+for freq in frequencies_list:
+    print(f"{freq['word']}: {freq['count']}")
--- stats.py
+++ stats.py
@@ -2,3 +2,30 @@
     
 def get_word_count(text):
     return len(text.split())
+
+def get_frequencies(text):
+    
+    frequencies = {}
+    for word in text:
+        if word.lower() in frequencies:
+            frequencies[word.lower()] += 1
+        else:
+            frequencies[word.lower()] = 1
+    return frequencies
+
+def get_only_character_frequency(words):
+    frequencies = {}
+    for word in words:
+        if word.isalpha():
+            if word.lower() in frequencies:
+                frequencies[word.lower()] += 1
+            else:
+                frequencies[word.lower()] = 1
+    return frequencies
+
+def get_frequency_list(words):
+    frequency_list = []
+    frequencies = get_only_character_frequency(words)
+    for word in frequencies:
+        frequency_list.append({"word": word, "count": frequencies[word]})
+    return frequency_list
